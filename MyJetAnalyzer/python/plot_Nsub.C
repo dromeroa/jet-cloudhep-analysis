b51f4045efb10ce590517cf48fc1112fd68be1bd
David Romero Abad <david.romero.abad@cern.ch>
2025-06-03 12:23:22 -0500

    Update plot_Nsub.C

--- MyJetAnalyzer/python/plot_Nsub.C
+++ MyJetAnalyzer/python/plot_Nsub.C
@@ -6,6 +6,9 @@
 #include <vector>
 
 void plot_Nsub() {
+
+     gROOT->SetBatch(kTRUE);
+
     // Abrir el archivo ROOT que contiene el TTree
     TFile *inputFile1 = TFile::Open("TreeFatjets.root", "READ");
     if (!inputFile1 || inputFile1->IsZombie()) {
@@ -21,20 +24,33 @@ void plot_Nsub() {
         return;
     }
 
+    Int_t nJet;
+
     // Crear punteros para los vectores de cada TTree
     std::vector<float> *tau1 = nullptr;
     std::vector<float> *tau2 = nullptr;
     std::vector<float> *tau3 = nullptr;
+    std::vector<float> *pt = nullptr;
+    std::vector<float> *prunedmass = nullptr;
+
+
 
     // Establecer las ramas en el TTree para los vectores
     tree1->SetBranchAddress("fatjet_tau1", &tau1); // Solo necesitas pasar los punteros a los vectores
     tree1->SetBranchAddress("fatjet_tau2", &tau2);
     tree1->SetBranchAddress("fatjet_tau3", &tau3);
- 
+    tree1->SetBranchAddress("fatjet_prunedmass", &prunedmass);
+    tree1->SetBranchAddress("fatjet_pt", &pt);
+    tree1->SetBranchAddress("numberfatjet", &nJet);
+
 
     // Crear un histograma para almacenar los resultados
     TH1F *h1 = new TH1F("histograma", "Histograma de tau21", 100, 0, 1);
     TH1F *h2 = new TH1F("histograma", "Histograma de tau31", 100, 0, 1); 
+    TH1F *h3 = new TH1F("histograma3", "Masa del jet", 100, 0, 500);
+    TH1F *h4 = new TH1F("h_nJet", "Numero de Jets por Evento;N° Jets;Eventos", 20, 0, 10);
+    TH1F *h5 = new TH1F("h_jetpt", "Jet p_{T};p_{T} [GeV];Número de jets", 100, 0, 500);
+
 
 
     // Leer los eventos y llenar el histograma con los elementos de tau1
@@ -44,13 +60,22 @@ void plot_Nsub() {
     for (Long64_t i = 0; i < nEntries; ++i) {
         tree1->GetEntry(i);  // Cargar el evento i
 
+            h4->Fill(nJet);
+
         // Verificar que tau1 no sea null antes de usarlo
-        if (tau1) {
+        if (tau1 && tau2 && tau3 && pt && prunedmass) {
             for (size_t j = 0; j < tau1->size(); ++j) {
                 // Llenar el histograma con los valores de tau1
-                h1->Fill(tau2->at(j)/tau1->at(j));
-                h2->Fill(tau3->at(j)/tau1->at(j));
-            }
+                if (tau1->at(j) > 0.0f) {
+                        h1->Fill(tau2->at(j)/tau1->at(j));
+                        h2->Fill(tau3->at(j)/tau1->at(j));
+                }
+
+                h3->Fill(prunedmass->at(j));
+                h5->Fill(pt->at(j));
+
+           } 
+
         }
     }
 
@@ -66,11 +91,17 @@ void plot_Nsub() {
     h2->Draw();
     c2->SaveAs("tau31.pdf");
 
+    TCanvas *c3 = new TCanvas("canvas3", "pruned mass", 800, 600);
+    h3->Draw();
+    c3->SaveAs("prunedMass.pdf");
 
+    TCanvas *c4 = new TCanvas("canvas4", "number of jets", 800, 600);
+    h4->Draw();
+    c4->SaveAs("Numjets.pdf");
 
-
-
-
+    TCanvas *c5 = new TCanvas("canvas5", "jet pt", 800, 600);
+    h5->Draw();
+    c5->SaveAs("jetPT.pdf");
 
 
     // Cerrar el archivo ROOT
